3cf14d56200761f9c17ebd0d73ada3195fff339c
ihor_acer <igorko323@gmail.com>
2021-04-07 20:24:44 +0300

    add my models demo

--- classification/demo.py
+++ classification/demo.py
@@ -1,8 +1,9 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
+import easygui
 
 
-def demo():
+def demoHuggingFace():
     tokenizer = AutoTokenizer.from_pretrained("bert-base-cased-finetuned-mrpc")
     model = AutoModelForSequenceClassification.from_pretrained("bert-base-cased-finetuned-mrpc")
 
@@ -29,5 +30,42 @@ def demo():
     for i in range(len(classes)):
         print(f"{classes[i]}: {round(not_paraphrase_results[i] * 100)}%")
 
+
+def demoMasters():
+    # model = torch.load("/home/ihor/University/DiplomaProject/Program/models/bert_base_cls_20210401-132024.pt")
+    model = torch.load("/home/ihor/University/DiplomaProject/Program/models/roberta_base_cls_20210401-180654.pt")
+    # model = torch.load("/home/ihor/University/DiplomaProject/Program/models/roberta_base_finetuned_semi_siam_20210402-190739.pt")
+    # model = torch.load("/home/ihor/University/DiplomaProject/Program/models/albert_base_v2_cls20210401-155540.pt")
+    device = torch.device("cuda")
+    model.to(device)
+
+    # sequence_0 = "The company HuggingFace is based in New York City"
+    # sequence_1 = "HuggingFace's headquarters are situated in Manhattan"
+    # sequence_2 = "Apples are especially bad for your health"
+
+    sequence_0 = easygui.enterbox("Write down the first sentence: ")
+    sequence_1 = easygui.enterbox("Write down the second sentence: ")
+    # sequence_2 = easygui.enterbox("Write down an arbitrary sentence: ")
+
+    # # bert
+    # threshold = 0.67940
+    # roberta
+    threshold = 0.32560
+    # # roberta finetuned semi-siam
+    # threshold = 0.50600
+    # # albert
+    # threshold = 0.22330
+
+    model.eval()
+    with torch.no_grad():
+        logits_par, _, _ = model.forward([sequence_0, sequence_1])
+        # logits_non_par, _, _ = model.forward([sequence_0, sequence_2])
+
+    print("Threshold for identifying a paraphrase: {:.5f}".format(threshold))
+    print("Sentences:\n1. {}\n2. {}\n--> Semantic similarity: {:.5f}".format(sequence_0, sequence_1, logits_par.cpu().numpy()))
+    # print("Sentences:\n1. {}\n2. {}\n--> Semantic similarity: {:.5f}".format(sequence_0, sequence_2, logits_non_par.cpu().numpy()))
+
+
 if __name__ == "__main__":
-    demo()
+    # demoHuggingFace()
+    demoMasters()
